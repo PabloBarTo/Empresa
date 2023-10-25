7ce5c74109d699426c049515f2d6e752601ca863
diagmatrix <manuelgbal1@gmail.com>
2023-10-25 18:48:22 +0200

    Añado nombre al jugador closes #9

--- src/models/jugador.ts
+++ src/models/jugador.ts
@@ -16,18 +16,25 @@ interface ValorMercado {
 }
 
 export class Jugador {
+    private readonly nombre: string;
     private readonly posicion: Posicion;
     private readonly puntuaciones: Desempeno[];
     private readonly valoresMercado: ValorMercado[];
 
     constructor(
+        nombre: string,
         posicion: Posicion, 
         puntuaciones: Desempeno[] = [], 
         valores: ValorMercado[] = []
     ) {
+        this.nombre = nombre;
         this.posicion = posicion;
         this.puntuaciones = puntuaciones;
         this.valoresMercado = valores;
+    }
+
+    getNombre(): string {
+        return this.nombre;
     }
 
     getPosicion(): Posicion {
