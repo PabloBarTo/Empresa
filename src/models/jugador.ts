de53d16306c22626e2c881bffeca2d760d93a897
diagmatrix <manuelgbal1@gmail.com>
2023-11-20 19:31:09 +0100

    Añado getters para jugador #9
    De esta manera no es necesario calcular a posteriori los valores

--- src/models/jugador.ts
+++ src/models/jugador.ts
@@ -17,4 +17,13 @@ export class Jugador {
         this.puntuaciones = puntuaciones;
         this.valoresMercado = valores;
     }
+
+    getValorActual(): number {
+        return this.valoresMercado[this.valoresMercado.length - 1];
+    }
+
+    getMediaValores(nDias: number): number {
+        const valores = this.valoresMercado.slice(-nDias);
+        return valores.reduce((a, b) => a + b, 0) / valores.length;
+    }
 }
