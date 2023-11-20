43f11191b38cc1c6433b89a9b2bf3087cf341a37
diagmatrix <manuelgbal1@gmail.com>
2023-11-20 19:50:53 +0100

    Documento entidad Mercado closes #10

--- src/models/mercado.ts
+++ src/models/mercado.ts
@@ -1,6 +1,10 @@
 import { Jugador } from "./jugador";
 
+/**
+ * Clase que representa el mercado de fichajes
+ */
 export class Mercado {
+  // Jugadores de la liga
   readonly jugadores: Jugador[];
 
   constructor(jugadores: Jugador[]) {
