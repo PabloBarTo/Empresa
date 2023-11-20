e43b472f5853489108b567281755794c9628bfa1
diagmatrix <manuelgbal1@gmail.com>
2023-11-20 19:06:19 +0100

    Elimino posición de Jugador
    Al mezclar los cambios hubo un error y reapareció

--- src/models/jugador.ts
+++ src/models/jugador.ts
@@ -15,22 +15,16 @@ export class Jugador {
 
     constructor(
         nombre: string,
-        posicion: Posicion, 
         puntuaciones: Desempeno[] = [], 
         valores: ValorMercado[] = []
     ) {
         this.nombre = nombre;
-        this.posicion = posicion;
         this.puntuaciones = puntuaciones;
         this.valoresMercado = valores;
     }
 
     getNombre(): string {
         return this.nombre;
-    }
-
-    getPosicion(): Posicion {
-        return this.posicion;
     }
 
     getPuntuaciones(): Desempeno[] {
