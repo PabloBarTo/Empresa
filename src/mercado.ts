1377d7bd4bd3c32d57d01c2de62105311bada202
PabloBarTo <pablo02barto@correo.ugr.es>
2023-11-21 16:39:08 +0100

    Implementación lógica de negocio #17
    Se ha creado la lógica de negocio que indica que jugadores del mercado debería fichar teniendo en cuenta el valor que tiene el jugador en ese momento y los puntos queu ha realizado en la última jornada closes #17

--- src/mercado.ts
+++ src/mercado.ts
@@ -12,4 +12,44 @@ export class Mercado {
         return this.jugadoresDisponibles;
     }
 
+    seleccionarJugadoresMaxPuntos(presupuestoMaximo: number): { jugadores: Jugador[], presupuestoRestante: number } {
+        const n = this.jugadoresDisponibles.length;
+
+        let mejorSeleccion: Jugador[] = [];
+        let mejorPuntuacion = 0;
+        let presupuesto_restante = presupuestoMaximo;
+
+        const calcularPuntuacion = (seleccion: Jugador[]): number => {
+            return seleccion.reduce((total, jugador) => total + jugador.getPuntuacionUltimaJornada(), 0);
+        };
+
+        const buscarCombinacion = (actual: number, seleccion: Jugador[], presupuestoRestante: number): void => {
+            if (actual === n) {
+                const puntuacionActual = calcularPuntuacion(seleccion);
+                if (puntuacionActual > mejorPuntuacion && presupuestoRestante >= 0) {
+                    mejorPuntuacion = puntuacionActual;
+                    mejorSeleccion = [...seleccion];
+                    presupuesto_restante = presupuestoRestante;
+                }
+                return;
+            }
+
+            seleccion.push(this.jugadoresDisponibles[actual]);
+            buscarCombinacion(actual + 1, seleccion, presupuestoRestante - this.jugadoresDisponibles[actual].getValorUltimaJornada());
+            seleccion.pop();  // Excluir el jugador actual en la selección
+
+            buscarCombinacion(actual + 1, seleccion, presupuestoRestante);
+        };
+
+        buscarCombinacion(0, [], presupuestoMaximo);
+
+        console.log("Jugadores seleccionados:", mejorSeleccion.map(jugador => jugador.nombre));
+        console.log("Presupuesto restante:", presupuesto_restante);
+
+        return {
+            jugadores: mejorSeleccion,
+            presupuestoRestante: presupuesto_restante
+        };
+    }
+    
 }
