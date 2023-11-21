23fc71ef3a22850a669ffa5e395bb840ddc619d7
PabloBarTo <pablo02barto@correo.ugr.es>
2023-11-21 16:02:12 +0100

    Ampliación clase Equipo #22
    Se han añadido unas variables y métodos que serán necesarios para la implementación de la lógica de negocio closes #22

--- src/equipo.ts
+++ src/equipo.ts
@@ -1,15 +1,29 @@
+import { Jugador } from './jugador.ts';
+import { Mercado } from './mercado.ts';
+
 export class Equipo {
-    constructor(
-        private nombre: string, 
-        private jugadores: string)
-    {}
+    private nombre  : string;
+    private jugadores : Jugador[];
+    private presupuesto : number;
+    private mercado : Mercado;
+
+    constructor(nombre: string, jugadores: Jugador[], presupuesto: number = 100000000, mercado: Mercado) {
+        this.nombre = nombre;
+        this.jugadores = jugadores;
+        this.presupuesto = presupuesto;
+        this.mercado = mercado;
+    }
 
     getNombre(): string {
         return this.nombre;
     }
 
-    getJugadores(): string {
+    getJugadores(): Jugador[] {
         return this.jugadores;
+    }
+
+    getPresupuesto(): number {
+        return this.presupuesto;
     }
 
 }
